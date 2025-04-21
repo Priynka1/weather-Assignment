a6e6aa8631697e3fe52332e8b2b527f0f8493d8c
priyanka jamwal <jamwalpriyanka02@gmail.com>
2025-04-21 13:16:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const weatherData = {
+  "himachal pradesh": {
+    temp: 20,
+    condition: "sunny",
+    location: "himachal pradesh",
+  },
+
+  delhi: {
+    temp: 40,
+    condition: "hot",
+    location: "delhi",
+  },
+  chandigarh: {
+    temp: 30,
+    condition: "hot",
+    location: "chandigarh",
+  },
+};
+
+const searchbtn = document.querySelector("#searchbtn");
+searchbtn.addEventListener("click", () => {
+  const city = document.getElementById("inputBox");
+  const inputCity = city.value;
+  city.value = "";
+
+  const result = document.getElementById("result");
+
+  if (inputCity) {
+    const data = weatherData[inputCity];
+    result.innerHTML = `
+      <h3> ${inputCity.toUpperCase()}</h3>
+      <p> ${data.temp} °C</p>
+      <p> ${data.condition}</p>
+      <p> ${data.location}</p>
+      
+    `;
+  } else {
+    result.innerHTML = `<p>Weather data for "${inputCity.value}" not found.</p>`;
+  }
+});
